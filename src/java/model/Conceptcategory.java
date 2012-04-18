00c703b2e99155b5e53581a657e3ae0d5e941882
lusv <lusv@live.com>
2012-04-18 14:39:27 -0600

    Generated Identity Beans from lel database

---
+++ src/java/model/Conceptcategory.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author lu
+ */
+@Entity
+@Table(name = "conceptcategory")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Conceptcategory.findAll", query = "SELECT c FROM Conceptcategory c"),
+    @NamedQuery(name = "Conceptcategory.findById", query = "SELECT c FROM Conceptcategory c WHERE c.id = :id"),
+    @NamedQuery(name = "Conceptcategory.findByConceptId", query = "SELECT c FROM Conceptcategory c WHERE c.conceptId = :conceptId")})
+public class Conceptcategory implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "conceptId")
+    private Integer conceptId;
+    @JoinColumn(name = "ConceptCategoryDef_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Conceptcategorydef conceptCategoryDefid;
+
+    public Conceptcategory() {
+    }
+
+    public Conceptcategory(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getConceptId() {
+        return conceptId;
+    }
+
+    public void setConceptId(Integer conceptId) {
+        this.conceptId = conceptId;
+    }
+
+    public Conceptcategorydef getConceptCategoryDefid() {
+        return conceptCategoryDefid;
+    }
+
+    public void setConceptCategoryDefid(Conceptcategorydef conceptCategoryDefid) {
+        this.conceptCategoryDefid = conceptCategoryDefid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Conceptcategory)) {
+            return false;
+        }
+        Conceptcategory other = (Conceptcategory) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Conceptcategory[ id=" + id + " ]";
+    }
+    
+}
