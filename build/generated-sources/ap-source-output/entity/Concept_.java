ad437b89d91543cf504157f7db844032ba43460b
lusv <lusv@live.com>
2012-05-28 12:45:10 -0600

    Refactor: database key-strings values;

--- build/generated-sources/ap-source-output/entity/Concept_.java
+++ build/generated-sources/ap-source-output/entity/Concept_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-05-25T13:08:32")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-05-28T12:37:11")
 @StaticMetamodel(Concept.class)
 public class Concept_ { 
 
